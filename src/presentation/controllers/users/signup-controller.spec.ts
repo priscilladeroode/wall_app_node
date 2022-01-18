16d67f1a7e601ee5aa759f0cf40660e80cc70e4f
Priscilla de Roode <priscilladeroode@gmail.com>
2022-01-18 20:39:45 -0300

    test: add customized error for missing param

--- src/presentation/controllers/users/signup-controller.spec.ts
+++ src/presentation/controllers/users/signup-controller.spec.ts
@@ -1,3 +1,4 @@
+import { MissingParamError } from '../../errors/missing-param-error'
 import { SignUpController } from './signup-controller'
 
 describe('Add Post Controller', () => {
@@ -12,7 +13,7 @@ describe('Add Post Controller', () => {
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+    expect(httpResponse.body).toEqual(new MissingParamError('name'))
   })
 
   test('Shoud return 400 if no email is provided', async () => {
@@ -26,6 +27,6 @@ describe('Add Post Controller', () => {
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
+    expect(httpResponse.body).toEqual(new MissingParamError('email'))
   })
 })
