b26a27829f55dccc95ca75fbf0ba1641c6a61650
Priscilla de Roode <priscilladeroode@gmail.com>
2022-01-18 20:34:17 -0300

    feat: ensure SignUpController.handle implement correct interfaces

--- src/presentation/controllers/users/signup-controller.ts
+++ src/presentation/controllers/users/signup-controller.ts
@@ -1,16 +1,13 @@
+import { badRequest } from '../../helpers/http'
+import { HttpRequest, HttpResponse } from '../../protocols/http'
+
 export class SignUpController {
-  handle (httpRequest: any): any {
+  handle (httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.name) {
-      return {
-        statusCode: 400,
-        body: new Error('Missing param: name')
-      }
+      return badRequest(new Error('Missing param: name'))
     }
     if (!httpRequest.body.email) {
-      return {
-        statusCode: 400,
-        body: new Error('Missing param: email')
-      }
+      return badRequest(new Error('Missing param: email'))
     }
   }
 }
