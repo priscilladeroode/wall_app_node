15a98f7a748a894d4ee741c152e78aa30c47f0ec
Priscilla de Roode <priscilladeroode@gmail.com>
2022-01-20 18:09:00 -0300

    test: ensure ValidationComposite dont return if validations succeeds

--- src/validations/validators/validation-composite.spec.ts
+++ src/validations/validators/validation-composite.spec.ts
@@ -49,4 +49,10 @@ describe('Validation Composite', () => {
     const error = sut.validate({ [field]: faker.random.word() })
     expect(error).toEqual(new Error(''))
   })
+
+  test('Should not return if validations succeeds', () => {
+    const { sut } = makeSut()
+    const error = sut.validate({ [field]: faker.random.word() })
+    expect(error).toBeFalsy()
+  })
 })
