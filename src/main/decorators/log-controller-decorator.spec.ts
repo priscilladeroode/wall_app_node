400dd76457359532b4e9196159c03b14beb7bbb5
Priscilla de Roode <priscilladeroode@gmail.com>
2022-01-20 09:12:27 -0300

    refactor: move sut creation to a factory helper method

--- src/main/decorators/log-controller-decorator.spec.ts
+++ src/main/decorators/log-controller-decorator.spec.ts
@@ -7,26 +7,43 @@ import {
   HttpResponse
 } from '../../presentation/protocols'
 
+interface SutTypes {
+  sut: LogControllerDecorator
+  controllerStub: Controller
+}
+
+const makeController = (): Controller => {
+  class ControllerStub implements Controller {
+    async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+      const password = faker.internet.password()
+      const httpResponse: HttpResponse = {
+        statusCode: 400,
+        body: {
+          name: faker.name.findName(),
+          email: faker.internet.email(),
+          password: password,
+          passwordConfirmation: password
+        }
+      }
+      return await Promise.resolve(httpResponse)
+    }
+  }
+  return new ControllerStub()
+}
+
+const makeSut = (): SutTypes => {
+  const controllerStub = makeController()
+  const sut = new LogControllerDecorator(controllerStub)
+  return {
+    sut,
+    controllerStub
+  }
+}
+
 describe('LogControllerDecorator', () => {
   test('Should call controller handle', async () => {
-    class ControllerStub implements Controller {
-      async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-        const password = faker.internet.password()
-        const httpResponse: HttpResponse = {
-          statusCode: 400,
-          body: {
-            name: faker.name.findName(),
-            email: faker.internet.email(),
-            password: password,
-            passwordConfirmation: password
-          }
-        }
-        return await Promise.resolve(httpResponse)
-      }
-    }
-    const controllerStub = new ControllerStub()
+    const { sut, controllerStub } = makeSut()
     const handleSpy = jest.spyOn(controllerStub, 'handle')
-    const sut = new LogControllerDecorator(controllerStub)
     const password = faker.internet.password()
     const httpRequest = {
       body: {
