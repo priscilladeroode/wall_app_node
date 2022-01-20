958840ad09240b4f1d7b557375caffbff0610773
Priscilla de Roode <priscilladeroode@gmail.com>
2022-01-19 22:44:36 -0300

    feat: add SignUpController factory

---
+++ src/main/factories/controllers/add-account-controller-factory.ts
@@ -0,0 +1,16 @@
+import { DBAddAccount } from '../../../data/usecases/users/db-add-account'
+import { BcryptAdapter } from '../../../infra/cryptography/bcrypt-adapter'
+import { UserMongoRepository } from '../../../infra/db/mongodb/user-mongo-repository'
+import { SignUpController } from '../../../presentation/controllers/users/signup-controller'
+import { Controller } from '../../../presentation/protocols'
+import { EmailValidatorAdapter } from '../../../validations/validators/email-validator'
+
+export const makeSignUpController = (): Controller => {
+  const salt = 12
+  const adapter = new EmailValidatorAdapter()
+  const encrypter = new BcryptAdapter(salt)
+  const repository = new UserMongoRepository()
+  const usecase = new DBAddAccount(encrypter, repository)
+  const controller = new SignUpController(adapter, usecase)
+  return controller
+}
