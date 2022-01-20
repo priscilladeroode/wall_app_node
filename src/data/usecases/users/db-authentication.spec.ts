f322de52344201154ff82db9a673597db523400d
Priscilla de Roode <priscilladeroode@gmail.com>
2022-01-20 20:50:27 -0300

    test: ensure DBAuthentication throws if LoadAccountByEmailRepository throws

--- src/data/usecases/users/db-authentication.spec.ts
+++ src/data/usecases/users/db-authentication.spec.ts
@@ -1,33 +1,64 @@
 import faker from 'faker'
+import { AuthenticationRequestModel } from '../../models/users/authentication-request-model'
 import { LoadAccountRequestModel } from '../../models/users/load-account-request-model'
 import { LoadAccountResponseModel } from '../../models/users/load-account-response-model'
 import { LoadAccountByEmailRepository } from '../../protocols/db/users/load-account-by-email-repository'
 import { DBAuthentication } from './db-authentication'
 
+const email = faker.internet.email()
+
+const makeFakeAccount = (): LoadAccountResponseModel => ({
+  id: 'any_id',
+  name: 'any_name',
+  email: email,
+  password: 'any_password'
+})
+
+const makeFakeRequest = (): AuthenticationRequestModel => ({
+  email: email,
+  password: faker.internet.password()
+})
+
+const makeLoadAccountByEmailRepository = (): LoadAccountByEmailRepository => {
+  class LoadAccountByEmailRepositoryStub
+  implements LoadAccountByEmailRepository {
+    async load (
+      accountData: LoadAccountRequestModel
+    ): Promise<LoadAccountResponseModel> {
+      return await Promise.resolve(makeFakeAccount())
+    }
+  }
+  return new LoadAccountByEmailRepositoryStub()
+}
+
+interface SutTypes {
+  sut: DBAuthentication
+  loadAccountByEmailRepositoryStub: LoadAccountByEmailRepository
+}
+
+const makeSut = (): SutTypes => {
+  const loadAccountByEmailRepositoryStub = makeLoadAccountByEmailRepository()
+  const sut = new DBAuthentication(loadAccountByEmailRepositoryStub)
+  return {
+    sut,
+    loadAccountByEmailRepositoryStub
+  }
+}
+
 describe('DBAuthentication', () => {
   test('Should call encrypter with correct password', async () => {
-    class LoadAccountByEmailRepositoryStub
-    implements LoadAccountByEmailRepository {
-      async load (
-        accountData: LoadAccountRequestModel
-      ): Promise<LoadAccountResponseModel> {
-        const response: LoadAccountResponseModel = {
-          id: 'any_id',
-          name: 'any_name',
-          email: 'any_email',
-          password: 'any_password'
-        }
-        return await Promise.resolve(response)
-      }
-    }
-    const repository = new LoadAccountByEmailRepositoryStub()
-    const sut = new DBAuthentication(repository)
-    const loadSpy = jest.spyOn(repository, 'load')
-    const email = faker.internet.email()
-    await sut.auth({
-      email: email,
-      password: faker.internet.password()
-    })
+    const { sut, loadAccountByEmailRepositoryStub } = makeSut()
+    const loadSpy = jest.spyOn(loadAccountByEmailRepositoryStub, 'load')
+    await sut.auth(makeFakeRequest())
     expect(loadSpy).toHaveBeenCalledWith({ email: email })
+  })
+
+  test('Should throw if LoadAccountByEmailRepository throws', async () => {
+    const { sut, loadAccountByEmailRepositoryStub } = makeSut()
+    jest
+      .spyOn(loadAccountByEmailRepositoryStub, 'load')
+      .mockReturnValueOnce(Promise.reject(new Error()))
+    const promise = sut.auth(makeFakeRequest())
+    await expect(promise).rejects.toThrow()
   })
 })
