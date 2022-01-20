e07f1a75493f847c0e8cf44a827bd53de5a26e2b
Priscilla de Roode <priscilladeroode@gmail.com>
2022-01-20 17:21:13 -0300

    refactor: move EmailValidation to the ValidationComposite

--- src/main/factories/validations/signup-validation-factory.spec.ts
+++ src/main/factories/validations/signup-validation-factory.spec.ts
@@ -1,10 +1,21 @@
 import { Validation } from '../../../presentation/protocols/validation'
+import { EmailValidator } from '../../../validations/protocols/email-validator'
 import { CompareFieldsValidation } from '../../../validations/validators/compare-fields-validation'
+import { EmailValidation } from '../../../validations/validators/email-validation'
 import { RequiredFieldValidation } from '../../../validations/validators/required-field-validation'
 import { ValidationComposite } from '../../../validations/validators/validation-composite'
 import { makeSignUpValidation } from './signup-validation-factory'
 
 jest.mock('../../../validations/validators/validation-composite')
+
+const makeEmailValidator = (): EmailValidator => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid (email: string): boolean {
+      return true
+    }
+  }
+  return new EmailValidatorStub()
+}
 
 describe('SignUpValidation Factory', () => {
   test('Should call ValidationComposite with all validations', () => {
@@ -17,6 +28,8 @@ describe('SignUpValidation Factory', () => {
     validations.push(
       new CompareFieldsValidation('password', 'passwordConfirmation')
     )
+
+    validations.push(new EmailValidation('email', makeEmailValidator()))
     expect(ValidationComposite).toHaveBeenCalledWith(validations)
   })
 })
--- src/presentation/controllers/users/signup-controller.ts
+++ src/presentation/controllers/users/signup-controller.ts
@@ -1,13 +1,10 @@
 import { AddAccount } from '../../../domain/usecases/users'
-import { EmailValidator } from '../../../validations/protocols/email-validator'
-import { InvalidParamError } from '../../errors'
 import { badRequest, serverError, ok } from '../../helpers/http'
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
 import { Validation } from '../../protocols/validation'
 
 export class SignUpController implements Controller {
   constructor (
-    private readonly emailValidator: EmailValidator,
     private readonly addAccount: AddAccount,
     private readonly validation: Validation
   ) {}
@@ -25,10 +22,6 @@ export class SignUpController implements Controller {
 
       const { name, password, email } = data
 
-      const isValid = this.emailValidator.isValid(email)
-      if (!isValid) {
-        return badRequest(new InvalidParamError('email'))
-      }
       const account = await this.addAccount.add({
         name,
         email,
