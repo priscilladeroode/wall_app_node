184776b06fc268b1fef60ce03b45ed4745448826
Priscilla de Roode <priscilladeroode@gmail.com>
2022-01-21 19:33:55 -0300

    test: ensure if axios SengGridEmailRepository throws if axios throws

--- src/infra/services/send-grid-email-repository.spec.ts
+++ src/infra/services/send-grid-email-repository.spec.ts
@@ -1,5 +1,7 @@
 import { SendGridEmailRepository } from './send-grid-email-repository'
 
+import axios from 'axios'
+
 jest.mock('axios', () => ({
   async post (): Promise<string> {
     return await Promise.resolve('')
@@ -10,6 +12,10 @@ const makeSut = (): SendGridEmailRepository => {
   return new SendGridEmailRepository()
 }
 
+const throwError = (): never => {
+  throw new Error()
+}
+
 const email = 'any_email@gmail.com'
 const name = 'any_name'
 
@@ -18,5 +24,12 @@ describe('Send Grid Email Repository', () => {
     const sut = makeSut()
     const result = await sut.send(email, name)
     expect(result).toBeFalsy()
+  })
+
+  test('Should throw if axios throw', async () => {
+    const sut = makeSut()
+    jest.spyOn(axios, 'post').mockImplementationOnce(throwError)
+    const promise = sut.send(email, name)
+    await expect(promise).rejects.toThrow()
   })
 })
