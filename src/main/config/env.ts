e87a85615d111b8ec0387ea22536be976f845ed3
Priscilla de Roode <priscilladeroode@gmail.com>
2022-01-21 01:21:14 -0300

    feat: add SignIn factory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,6 @@
 export default {
   mongoUrl:
     process.env.MONGO_URL || 'mongodb://localhost:27017/wall-app-backend',
-  port: process.env.PORT || 5050
+  port: process.env.PORT || 5050,
+  jwtSecret: process.env.JWT_SECRET || 'PRtA1988*2305=pEFG'
 }
--- src/main/factories/controllers/signin-controller-factory.ts
+++ src/main/factories/controllers/signin-controller-factory.ts
@@ -1,11 +1,24 @@
 import { DBAuthentication } from '../../../data/usecases/users/db-authentication'
+import { BcryptAdapter } from '../../../infra/cryptography/bcrypt-adapter'
+import { JwtAdapter } from '../../../infra/cryptography/jwt-adapter'
+import { UserMongoRepository } from '../../../infra/db/mongodb/user-mongo-repository'
 import { SignInController } from '../../../presentation/controllers/users/signin-controller'
 import { Controller } from '../../../presentation/protocols'
+import env from '../../config/env'
 import { makeLogControllerDecorator } from '../decorators/log-controller-decorator-factory'
 import { makeSignInValidation } from '../validations/signin-validation-factory'
 
 export const makeSignInController = (): Controller => {
-  const usecase = new DBAuthentication(encrypter, repository)
+  const repository = new UserMongoRepository()
+  const salt = 12
+  const hashComparer = new BcryptAdapter(salt)
+  const encrypter = new JwtAdapter(env.jwtSecret)
+  const usecase = new DBAuthentication(
+    repository,
+    hashComparer,
+    encrypter,
+    repository
+  )
   const validation = makeSignInValidation()
   const controller = new SignInController(validation, usecase)
   return makeLogControllerDecorator(controller)
