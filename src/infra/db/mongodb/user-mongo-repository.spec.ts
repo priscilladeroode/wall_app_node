aae4390cfe45b4ea9e52fec6f8979a565b2d0982
Priscilla de Roode <priscilladeroode@gmail.com>
2022-01-20 23:58:50 -0300

    test: ensure UserMongoRepository returns an account on loadByEmail succeed

--- src/infra/db/mongodb/user-mongo-repository.spec.ts
+++ src/infra/db/mongodb/user-mongo-repository.spec.ts
@@ -1,10 +1,31 @@
 import faker from 'faker'
+import { Collection } from 'mongodb'
+import {
+  AddAccountRequestModel,
+  LoadAccountRequestModel
+} from '../../../data/models/users'
 import { MongoHelper } from '../../helpers/mongo-helper'
 import { UserMongoRepository } from './user-mongo-repository'
+
+const fakeName = faker.name.findName()
+const fakeEmail = faker.internet.email()
+const fakePassword = faker.internet.password()
+
+const loadByEmailRequestModel: LoadAccountRequestModel = {
+  email: fakeEmail
+}
+
+const accountRequestModel: AddAccountRequestModel = {
+  name: fakeName,
+  email: fakeEmail,
+  password: fakePassword
+}
 
 const makeSut = (): UserMongoRepository => {
   return new UserMongoRepository()
 }
+
+let collection: Collection
 
 describe('User Mongo Repository', () => {
   beforeAll(async () => {
@@ -16,19 +37,25 @@ describe('User Mongo Repository', () => {
   })
 
   beforeEach(async () => {
-    const collection = MongoHelper.getCollection('users')
+    collection = MongoHelper.getCollection('users')
     await collection.deleteMany({})
   })
 
-  test('Should return an account on success', async () => {
+  test('Should return an account on add success', async () => {
     const sut = makeSut()
-    const accountModel = {
-      name: faker.name.findName(),
-      email: faker.internet.email(),
-      password: faker.internet.password()
-    }
-    const result = await sut.add(accountModel)
+    const result = await sut.add(accountRequestModel)
     expect(result).toBeTruthy()
     expect(result.message).toBeTruthy()
+  })
+
+  test('Should return an account on loadByEmail success', async () => {
+    const sut = makeSut()
+    await collection.insertOne(accountRequestModel)
+    const result = await sut.loadByEmail(loadByEmailRequestModel)
+    expect(result).toBeTruthy()
+    expect(result.id).toBeTruthy()
+    expect(result.name).toBe(accountRequestModel.name)
+    expect(result.email).toBe(accountRequestModel.email)
+    expect(result.password).toBe(accountRequestModel.password)
   })
 })
