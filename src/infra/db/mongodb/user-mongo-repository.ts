acf7a03e04bcd67186e3293f325fc08fda689977
Priscilla de Roode <priscilladeroode@gmail.com>
2022-01-20 23:58:23 -0300

    feat: ensure UserMongoRepository returns an account on loadByEmail succeed

--- src/infra/db/mongodb/user-mongo-repository.ts
+++ src/infra/db/mongodb/user-mongo-repository.ts
@@ -1,16 +1,37 @@
 import {
   AddAccountRequestModel,
-  AddAccountResponseModel
+  AddAccountResponseModel,
+  LoadAccountRequestModel,
+  LoadAccountResponseModel
 } from '../../../data/models/users'
 import { AddAccountRepository } from '../../../data/protocols/db/users/add-account-repository'
+import { LoadAccountByEmailRepository } from '../../../data/protocols/db/users/load-account-by-email-repository'
 import { MongoHelper } from '../../helpers/mongo-helper'
 
-export class UserMongoRepository implements AddAccountRepository {
+export class UserMongoRepository
+implements AddAccountRepository, LoadAccountByEmailRepository {
   async add (
     accountData: AddAccountRequestModel
   ): Promise<AddAccountResponseModel> {
     const collection = MongoHelper.getCollection('users')
     await collection.insertOne(accountData)
-    return { message: 'User successfully registered' }
+    const result: AddAccountResponseModel = {
+      message: 'User successfully registered'
+    }
+    return result
+  }
+
+  async loadByEmail (
+    accountData: LoadAccountRequestModel
+  ): Promise<LoadAccountResponseModel> {
+    const collection = MongoHelper.getCollection('users')
+    const account = await collection.findOne({ email: accountData.email })
+    const result: LoadAccountResponseModel = {
+      id: account._id.toHexString(),
+      name: account.name,
+      email: account.email,
+      password: account.password
+    }
+    return result
   }
 }
