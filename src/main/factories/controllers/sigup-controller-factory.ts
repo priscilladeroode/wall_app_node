557e625ada931d49750daeb161321c18ede50493
Priscilla de Roode <priscilladeroode@gmail.com>
2022-01-21 19:31:42 -0300

    feat: ensure an email is send after account creation

--- src/main/factories/controllers/sigup-controller-factory.ts
+++ src/main/factories/controllers/sigup-controller-factory.ts
@@ -1,6 +1,7 @@
 import { DBAddAccount } from '../../../data/usecases/users/db-add-account'
 import { BcryptAdapter } from '../../../infra/cryptography/bcrypt-adapter'
 import { UserMongoRepository } from '../../../infra/db/mongodb/user-mongo-repository'
+import { SendGridEmailRepository } from '../../../infra/services/send-grid-email-repository'
 import { SignUpController } from '../../../presentation/controllers/users/signup-controller'
 import { Controller } from '../../../presentation/protocols'
 import { makeLogControllerDecorator } from '../decorators/log-controller-decorator-factory'
@@ -10,7 +11,8 @@ export const makeSignUpController = (): Controller => {
   const salt = 12
   const hasher = new BcryptAdapter(salt)
   const repository = new UserMongoRepository()
-  const usecase = new DBAddAccount(hasher, repository)
+  const sendEmail = new SendGridEmailRepository()
+  const usecase = new DBAddAccount(hasher, repository, sendEmail)
   const validation = makeSignUpValidation()
   const controller = new SignUpController(usecase, validation)
   return makeLogControllerDecorator(controller)
