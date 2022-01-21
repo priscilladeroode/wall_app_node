5ecf06896eda9d6ef1660331c1e4e57bfa0bb9d9
Priscilla de Roode <priscilladeroode@gmail.com>
2022-01-20 23:08:34 -0300

    test: ensure JwtAdapter calls sign with correct values

---
+++ src/infra/cryptography/jwt-adapter.spec.ts
@@ -0,0 +1,22 @@
+import { JwtAdapter } from './jwt-adapter'
+
+import jwt from 'jsonwebtoken'
+
+jest.mock('jsonwebtoken', () => ({
+  async sign (): Promise<string> {
+    return 'any_token'
+  }
+}))
+
+const makeSut = (): JwtAdapter => {
+  return new JwtAdapter('secret')
+}
+
+describe('Jwt Adapter', () => {
+  test('Should call sign with correct values', async () => {
+    const sut = makeSut()
+    const signSpy = jest.spyOn(jwt, 'sign')
+    await sut.encrypt('any_id')
+    expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret')
+  })
+})
