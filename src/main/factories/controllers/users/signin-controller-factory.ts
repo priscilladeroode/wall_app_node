b151836b397fda6103516b1b61923df00e275e1a
Priscilla de Roode <priscilladeroode@gmail.com>
2022-01-22 01:52:12 -0300

    refactor: move user factories to users folders

---
+++ src/main/factories/controllers/users/signin-controller-factory.ts
@@ -0,0 +1,12 @@
+import { SignInController } from '../../../../presentation/controllers/users/signin-controller'
+import { Controller } from '../../../../presentation/protocols'
+import { makeLogControllerDecorator } from '../../decorators/log-controller-decorator-factory'
+import { makeAuthenticationUseCase } from '../../usecases/users/authentication-usecase-factory'
+import { makeSignInValidation } from '../../validations/users/signin-validation-factory'
+
+export const makeSignInController = (): Controller => {
+  const authenticationUseCase = makeAuthenticationUseCase()
+  const validation = makeSignInValidation()
+  const controller = new SignInController(validation, authenticationUseCase)
+  return makeLogControllerDecorator(controller)
+}
