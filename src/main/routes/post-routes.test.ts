3c3f316c4d9f703d044906ad5b44d24cbc68a3a5
Priscilla de Roode <priscilladeroode@gmail.com>
2022-01-22 15:17:39 -0300

    test: ensure when create a post return 201 on success

--- src/main/routes/post-routes.test.ts
+++ src/main/routes/post-routes.test.ts
@@ -1,13 +1,21 @@
 import app from '../../../src/main/config/app'
 import { MongoHelper } from '../../infra/helpers/mongo-helper'
-import { Collection } from 'mongodb'
+import { Collection, ObjectId } from 'mongodb'
 
 import faker from 'faker'
 import request from 'supertest'
+import { hash } from 'bcrypt'
+import { sign } from 'jsonwebtoken'
+import env from '../config/env'
 
-let collection: Collection
+let postsCollection: Collection
+let usersCollection: Collection
 
-describe('SignUp Routes', () => {
+const password = faker.internet.password()
+const email = faker.internet.email()
+const name = faker.name.findName()
+
+describe('Posts Routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
   })
@@ -17,11 +25,13 @@ describe('SignUp Routes', () => {
   })
 
   beforeEach(async () => {
-    collection = MongoHelper.getCollection('posts')
-    await collection.deleteMany({})
+    postsCollection = MongoHelper.getCollection('posts')
+    await postsCollection.deleteMany({})
+    usersCollection = MongoHelper.getCollection('users')
+    await usersCollection.deleteMany({})
   })
 
-  test('Should return a 403 on create post', async () => {
+  test('Should return a 403 on create post without accessToken', async () => {
     await request(app)
       .post('/api/posts')
       .send({
@@ -29,5 +39,32 @@ describe('SignUp Routes', () => {
         content: faker.lorem.paragraphs()
       })
       .expect(403)
+  })
+
+  test('Should return a 201 on create post', async () => {
+    const hashedPassword = await hash(password, 12)
+    const result = await usersCollection.insertOne({
+      name,
+      email,
+      password: hashedPassword
+    })
+    const id = result.insertedId.toHexString()
+    const accessToken = sign({ id }, env.jwtSecret)
+    await usersCollection.updateOne(
+      { _id: new ObjectId(id) },
+      {
+        $set: {
+          accessToken
+        }
+      }
+    )
+    await request(app)
+      .post('/api/posts')
+      .set('x-access-token', accessToken)
+      .send({
+        title: faker.lorem.sentence(),
+        content: faker.lorem.paragraphs()
+      })
+      .expect(201)
   })
 })
