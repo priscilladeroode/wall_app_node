71b14b1d50fde82a1c7aa4b89d004efea52ca175
Priscilla de Roode <priscilladeroode@gmail.com>
2022-01-22 00:20:19 -0300

    test: ensure AddPostController calls Validation with correct values

---
+++ src/presentation/controllers/posts/add-post-controller.spec.ts
@@ -0,0 +1,51 @@
+import { Validation } from '../../protocols/validation'
+import { AddPostController } from './add-post-controller'
+
+import faker from 'faker'
+import { HttpRequest } from '../../protocols'
+
+type SutTypes = {
+  sut: AddPostController
+  validationStub: Validation
+}
+
+const title = faker.lorem.sentence()
+const content = faker.lorem.paragraphs()
+const uid = faker.datatype.uuid()
+
+const request: HttpRequest = {
+  body: {
+    title,
+    content,
+    uid
+  }
+}
+
+const makeValidation = (): Validation => {
+  class ValidationStub implements Validation {
+    validate (input: any): Error {
+      return null
+    }
+  }
+  return new ValidationStub()
+}
+
+const makeSut = (): SutTypes => {
+  const validationStub = makeValidation()
+  const sut = new AddPostController(validationStub)
+  return {
+    sut,
+    validationStub
+  }
+}
+
+describe('AddPostController', () => {
+  describe('Validation', () => {
+    test('Shoud call Validation with correct values', async () => {
+      const { sut, validationStub } = makeSut()
+      const validateSpy = jest.spyOn(validationStub, 'validate')
+      await sut.handle(request)
+      expect(validateSpy).toHaveBeenCalledWith(request.body)
+    })
+  })
+})
