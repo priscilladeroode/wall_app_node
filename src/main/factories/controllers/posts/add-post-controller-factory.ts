6a5ef33645968bff8ac9a79d22692425f765060b
Priscilla de Roode <priscilladeroode@gmail.com>
2022-01-22 02:02:13 -0300

    feat: add factory method to AddPostController

---
+++ src/main/factories/controllers/posts/add-post-controller-factory.ts
@@ -0,0 +1,12 @@
+import { AddPostController } from '../../../../presentation/controllers/posts/add-post-controller'
+import { Controller } from '../../../../presentation/protocols'
+import { makeLogControllerDecorator } from '../../decorators/log-controller-decorator-factory'
+import { makeAddPostUseCase } from '../../usecases/posts/add-post-usecase-factory'
+import { makeAddPostValidation } from '../../validations/posts/add-post-validation-factory'
+
+export const makeAddPostController = (): Controller => {
+  const usecase = makeAddPostUseCase()
+  const validation = makeAddPostValidation()
+  const controller = new AddPostController(validation, usecase)
+  return makeLogControllerDecorator(controller)
+}
