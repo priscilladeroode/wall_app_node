76fbfe85e0518c08a1329949530b825e665e1efa
Priscilla de Roode <priscilladeroode@gmail.com>
2022-01-21 22:41:26 -0300

    feat: move AuthenticationUseCase creation to a separated file

--- src/main/factories/controllers/signin-controller-factory.ts
+++ src/main/factories/controllers/signin-controller-factory.ts
@@ -1,25 +1,12 @@
-import { DBAuthentication } from '../../../data/usecases/users/db-authentication'
-import { BcryptAdapter } from '../../../infra/cryptography/bcrypt-adapter'
-import { JwtAdapter } from '../../../infra/cryptography/jwt-adapter'
-import { UserMongoRepository } from '../../../infra/db/mongodb/user-mongo-repository'
 import { SignInController } from '../../../presentation/controllers/users/signin-controller'
 import { Controller } from '../../../presentation/protocols'
-import env from '../../config/env'
 import { makeLogControllerDecorator } from '../decorators/log-controller-decorator-factory'
+import { makeAuthenticationUseCase } from '../usecases/authentication-usecase-factory'
 import { makeSignInValidation } from '../validations/signin-validation-factory'
 
 export const makeSignInController = (): Controller => {
-  const repository = new UserMongoRepository()
-  const salt = 12
-  const hashComparer = new BcryptAdapter(salt)
-  const encrypter = new JwtAdapter(env.jwtSecret)
-  const usecase = new DBAuthentication(
-    repository,
-    hashComparer,
-    encrypter,
-    repository
-  )
+  const authenticationUseCase = makeAuthenticationUseCase()
   const validation = makeSignInValidation()
-  const controller = new SignInController(validation, usecase)
+  const controller = new SignInController(validation, authenticationUseCase)
   return makeLogControllerDecorator(controller)
 }
