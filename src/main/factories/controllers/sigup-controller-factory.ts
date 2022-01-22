904c1a845918c84a0a6110c60e99f17f0a28caa6
Priscilla de Roode <priscilladeroode@gmail.com>
2022-01-21 22:42:07 -0300

    feat: move AddAccountUseCase creation to a separated file

--- src/main/factories/controllers/sigup-controller-factory.ts
+++ src/main/factories/controllers/sigup-controller-factory.ts
@@ -1,19 +1,18 @@
-import { DBAddAccount } from '../../../data/usecases/users/db-add-account'
-import { BcryptAdapter } from '../../../infra/cryptography/bcrypt-adapter'
-import { UserMongoRepository } from '../../../infra/db/mongodb/user-mongo-repository'
-import { SendGridEmailRepository } from '../../../infra/services/send-grid-email-repository'
 import { SignUpController } from '../../../presentation/controllers/users/signup-controller'
 import { Controller } from '../../../presentation/protocols'
 import { makeLogControllerDecorator } from '../decorators/log-controller-decorator-factory'
+import { makeAddAccountUseCase } from '../usecases/add-account-usecase-factory'
+import { makeAuthenticationUseCase } from '../usecases/authentication-usecase-factory'
 import { makeSignUpValidation } from '../validations/signup-validation-factory'
 
 export const makeSignUpController = (): Controller => {
-  const salt = 12
-  const hasher = new BcryptAdapter(salt)
-  const repository = new UserMongoRepository()
-  const sendEmail = new SendGridEmailRepository()
-  const usecase = new DBAddAccount(hasher, repository, sendEmail)
+  const usecase = makeAddAccountUseCase()
   const validation = makeSignUpValidation()
-  const controller = new SignUpController(usecase, validation)
+  const authenticationUseCase = makeAuthenticationUseCase()
+  const controller = new SignUpController(
+    usecase,
+    validation,
+    authenticationUseCase
+  )
   return makeLogControllerDecorator(controller)
 }
