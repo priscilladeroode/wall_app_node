66b8a68480df6470dab56cf331b61dbf2d11ece2
Priscilla de Roode <priscilladeroode@gmail.com>
2022-01-21 23:45:25 -0300

    refactor: move each method to a describe

--- src/infra/db/mongodb/user-mongo-repository.spec.ts
+++ src/infra/db/mongodb/user-mongo-repository.spec.ts
@@ -2,6 +2,7 @@ import faker from 'faker'
 import { Collection } from 'mongodb'
 import {
   AddAccountRequestModel,
+  CheckAccountRequestModel,
   LoadAccountRequestModel
 } from '../../../data/models/users'
 import { UpdateAccessTokenRequestModel } from '../../../data/models/users/update-access-token-request-model'
@@ -13,6 +14,10 @@ const fakeEmail = faker.internet.email()
 const fakePassword = faker.internet.password()
 
 const loadByEmailRequestModel: LoadAccountRequestModel = {
+  email: fakeEmail
+}
+
+const checkByEmailRequestModel: CheckAccountRequestModel = {
   email: fakeEmail
 }
 
@@ -42,57 +47,63 @@ describe('User Mongo Repository', () => {
     await collection.deleteMany({})
   })
 
-  test('Should return an account on add success', async () => {
-    const sut = makeSut()
-    const result = await sut.add(accountRequestModel)
-    expect(result).toBeTruthy()
-    expect(result.registered).toBeTruthy()
-  })
-
-  test('Should return an account on loadByEmail success', async () => {
-    const sut = makeSut()
-    await collection.insertOne(accountRequestModel)
-    const result = await sut.loadByEmail(loadByEmailRequestModel)
-    expect(result).toBeTruthy()
-    expect(result.id).toBeTruthy()
-    expect(result.name).toBe(accountRequestModel.name)
-    expect(result.email).toBe(accountRequestModel.email)
-    expect(result.password).toBe(accountRequestModel.password)
-  })
-
-  test('Should return null if loadByEmail fails', async () => {
-    const sut = makeSut()
-    const result = await sut.loadByEmail(loadByEmailRequestModel)
-    expect(result).toBeFalsy()
-  })
-
-  test('Should update the account accessToken on success', async () => {
-    const sut = makeSut()
-    const account = await collection.insertOne(accountRequestModel)
-    const updateAccessTokenRequestModel: UpdateAccessTokenRequestModel = {
-      id: account.insertedId.toHexString(),
-      accessToken: 'any_accessToken'
-    }
-    await sut.updateAccessToken(updateAccessTokenRequestModel)
-    const result = await collection.findOne({
-      email: accountRequestModel.email
+  describe('add', () => {
+    test('Should return an account on add success', async () => {
+      const sut = makeSut()
+      const result = await sut.add(accountRequestModel)
+      expect(result).toBeTruthy()
+      expect(result.registered).toBeTruthy()
     })
-    expect(result).toBeTruthy()
-    expect(result.accessToken).toBe(updateAccessTokenRequestModel.accessToken)
+  })
+
+  describe('loadByEmail', () => {
+    test('Should return an account on loadByEmail success', async () => {
+      const sut = makeSut()
+      await collection.insertOne(accountRequestModel)
+      const result = await sut.loadByEmail(loadByEmailRequestModel)
+      expect(result).toBeTruthy()
+      expect(result.id).toBeTruthy()
+      expect(result.name).toBe(accountRequestModel.name)
+      expect(result.email).toBe(accountRequestModel.email)
+      expect(result.password).toBe(accountRequestModel.password)
+    })
+
+    test('Should return null if loadByEmail fails', async () => {
+      const sut = makeSut()
+      const result = await sut.loadByEmail(loadByEmailRequestModel)
+      expect(result).toBeFalsy()
+    })
+  })
+
+  describe('', () => {
+    test('Should update the account accessToken on success', async () => {
+      const sut = makeSut()
+      const account = await collection.insertOne(accountRequestModel)
+      const updateAccessTokenRequestModel: UpdateAccessTokenRequestModel = {
+        id: account.insertedId.toHexString(),
+        accessToken: 'any_accessToken'
+      }
+      await sut.updateAccessToken(updateAccessTokenRequestModel)
+      const result = await collection.findOne({
+        email: accountRequestModel.email
+      })
+      expect(result).toBeTruthy()
+      expect(result.accessToken).toBe(updateAccessTokenRequestModel.accessToken)
+    })
   })
 
   describe('CheckAccountByEmailRepository', () => {
     test('Should return true when checkByEmail finds account', async () => {
       const sut = makeSut()
       await collection.insertOne(accountRequestModel)
-      const result = await sut.checkByEmail(loadByEmailRequestModel)
+      const result = await sut.checkByEmail(checkByEmailRequestModel)
       expect(result).toBeTruthy()
       expect(result.exists).toBe(true)
     })
 
     test('Should return false when checkByEmail finds no account', async () => {
       const sut = makeSut()
-      const result = await sut.checkByEmail(loadByEmailRequestModel)
+      const result = await sut.checkByEmail(checkByEmailRequestModel)
       expect(result.exists).toBeFalsy()
     })
   })
