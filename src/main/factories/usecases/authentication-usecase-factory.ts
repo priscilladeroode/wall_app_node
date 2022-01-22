9b143ab6f426c642327a13956f5a309db9c5ae5d
Priscilla de Roode <priscilladeroode@gmail.com>
2022-01-21 22:41:26 -0300

    feat: move AuthenticationUseCase creation to a separated file

---
+++ src/main/factories/usecases/authentication-usecase-factory.ts
@@ -0,0 +1,14 @@
+import { DBAuthentication } from '../../../data/usecases/users/db-authentication'
+import { AuthenticationUseCase } from '../../../domain/usecases/users/authentication-usecase'
+import { BcryptAdapter } from '../../../infra/cryptography/bcrypt-adapter'
+import { JwtAdapter } from '../../../infra/cryptography/jwt-adapter'
+import { UserMongoRepository } from '../../../infra/db/mongodb/user-mongo-repository'
+import env from '../../config/env'
+
+export const makeAuthenticationUseCase = (): AuthenticationUseCase => {
+  const repository = new UserMongoRepository()
+  const salt = 12
+  const hashComparer = new BcryptAdapter(salt)
+  const encrypter = new JwtAdapter(env.jwtSecret)
+  return new DBAuthentication(repository, hashComparer, encrypter, repository)
+}
