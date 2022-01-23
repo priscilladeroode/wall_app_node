bb70b41ff81ea2cca7b43a3a43d17c025b89e84a
Priscilla de Roode <priscilladeroode@gmail.com>
2022-01-22 22:11:28 -0300

    test: ensure LoadPostsByUidController calls Validation with correct values

--- src/presentation/controllers/posts/load-posts-by-uid-controller.spec.ts
+++ src/presentation/controllers/posts/load-posts-by-uid-controller.spec.ts
@@ -8,9 +8,11 @@ import { ok, serverError } from '../../helpers/http'
 import faker from 'faker'
 import { LoadPostsByUidController } from './load-posts-by-uid-controller'
 import { LoadPostsByUidUseCase } from '../../../domain/usecases/posts/load-posts-by-uid-usecase'
+import { Validation } from '../../protocols/validation'
 
 type SutTypes = {
   sut: LoadPostsByUidController
+  validationStub: Validation
   loadPostsByUidUseCaseStub: LoadPostsByUidUseCase
 }
 const userId = faker.datatype.uuid()
@@ -38,37 +40,62 @@ const makeLoadPostsByUidUseCaseStub = (): LoadPostsByUidUseCase => {
   return new LoadPostsByUidUseCaseStub()
 }
 
+const makeValidation = (): Validation => {
+  class ValidationStub implements Validation {
+    validate (input: any): Error {
+      return null
+    }
+  }
+  return new ValidationStub()
+}
+
 const makeSut = (): SutTypes => {
+  const validationStub = makeValidation()
   const loadPostsByUidUseCaseStub = makeLoadPostsByUidUseCaseStub()
-  const sut = new LoadPostsByUidController(loadPostsByUidUseCaseStub)
+  const sut = new LoadPostsByUidController(
+    loadPostsByUidUseCaseStub,
+    validationStub
+  )
   return {
     sut,
+    validationStub,
     loadPostsByUidUseCaseStub
   }
 }
 
 describe('LoadPostsByUidController', () => {
-  test('Shoud return 500 if LoadPostsByUidUseCase throws', async () => {
-    const { sut, loadPostsByUidUseCaseStub } = makeSut()
-    jest
-      .spyOn(loadPostsByUidUseCaseStub, 'loadByUid')
-      .mockImplementationOnce(async () => {
-        return await Promise.reject(new Error())
-      })
-    const httpResponse = await sut.handle(request)
-    expect(httpResponse).toEqual(serverError(new ServerError(null)))
+  describe('', () => {
+    test('Shoud return 500 if LoadPostsByUidUseCase throws', async () => {
+      const { sut, loadPostsByUidUseCaseStub } = makeSut()
+      jest
+        .spyOn(loadPostsByUidUseCaseStub, 'loadByUid')
+        .mockImplementationOnce(async () => {
+          return await Promise.reject(new Error())
+        })
+      const httpResponse = await sut.handle(request)
+      expect(httpResponse).toEqual(serverError(new ServerError(null)))
+    })
+
+    test('Shoud return 200 if LoadPostsByUidUseCase returns posts', async () => {
+      const { sut } = makeSut()
+      const httpResponse = await sut.handle(request)
+      expect(httpResponse).toEqual(ok(loadPostsResponseEntity))
+    })
+
+    test('Shoud call LoadPostsByUidUseCase with correct values', async () => {
+      const { sut, loadPostsByUidUseCaseStub } = makeSut()
+      const loadByUidSpy = jest.spyOn(loadPostsByUidUseCaseStub, 'loadByUid')
+      await sut.handle(request)
+      expect(loadByUidSpy).toHaveBeenCalledWith({ uid: userId })
+    })
   })
 
-  test('Shoud return 200 if LoadPostsByUidUseCase returns posts', async () => {
-    const { sut } = makeSut()
-    const httpResponse = await sut.handle(request)
-    expect(httpResponse).toEqual(ok(loadPostsResponseEntity))
-  })
-
-  test('Shoud call LoadPostsByUidUseCase with correct values', async () => {
-    const { sut, loadPostsByUidUseCaseStub } = makeSut()
-    const loadByUidSpy = jest.spyOn(loadPostsByUidUseCaseStub, 'loadByUid')
-    await sut.handle(request)
-    expect(loadByUidSpy).toHaveBeenCalledWith({ uid: userId })
+  describe('Validation', () => {
+    test('Shoud call Validation with correct values', async () => {
+      const { sut, validationStub } = makeSut()
+      const validateSpy = jest.spyOn(validationStub, 'validate')
+      await sut.handle(request)
+      expect(validateSpy).toHaveBeenCalledWith(request.body)
+    })
   })
 })
