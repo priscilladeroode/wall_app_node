6a17322e69b9386463fbc7c3e48cc1ecf24e5b6f
Priscilla de Roode <priscilladeroode@gmail.com>
2022-01-23 03:08:58 -0300

    feat: ensure on loadById returns a post on success

--- src/infra/db/mongodb/post-mongo-repository.ts
+++ src/infra/db/mongodb/post-mongo-repository.ts
@@ -3,10 +3,12 @@ import {
   AddPostRequestModel,
   AddPostResponseModel,
   LoadPostsByUidRequestModel,
-  LoadPostsResponseModel
+  LoadPostsResponseModel,
+  PostModel
 } from '../../../data/models/posts'
 import { AddPostRepository } from '../../../data/protocols/db/posts/add-post-repository'
 import { LoadAllPostsRepository } from '../../../data/protocols/db/posts/load-all-posts-repository'
+import { LoadPostByIdRepository } from '../../../data/protocols/db/posts/load-post-by-id-respository'
 import { LoadPostsByUidRepository } from '../../../data/protocols/db/posts/load-posts-by-uid-repository'
 import { MongoHelper } from '../../helpers/mongo-helper'
 import { QueryBuilder } from '../../helpers/query-builder-helper'
@@ -15,7 +17,8 @@ export class PostMongoRepository
 implements
     AddPostRepository,
     LoadAllPostsRepository,
-    LoadPostsByUidRepository {
+    LoadPostsByUidRepository,
+    LoadPostByIdRepository {
   async add (post: AddPostRequestModel): Promise<AddPostResponseModel> {
     const collection = MongoHelper.getCollection('posts')
     const inserted = await collection.insertOne(post)
@@ -86,5 +89,34 @@ implements
       return MongoHelper.mapCollection(result)
     }
     return result as LoadPostsResponseModel
+  }
+
+  async loadById (model: string): Promise<PostModel> {
+    const collection = MongoHelper.getCollection('posts')
+    const query = new QueryBuilder()
+      .match({
+        _id: new ObjectId(model)
+      })
+      .lookup({
+        from: 'users',
+        foreignField: '_id',
+        localField: 'uid',
+        as: 'users'
+      })
+      .unwind({ path: '$users' })
+      .project({
+        id: '$_id',
+        title: '$title',
+        content: '$content',
+        createdBy: '$users.name',
+        createdAt: { $toDate: '$_id' }
+      })
+      .build()
+
+    const result = await collection.aggregate(query).toArray()
+    if (result.length > 0) {
+      return MongoHelper.map(result[0])
+    }
+    return null
   }
 }
