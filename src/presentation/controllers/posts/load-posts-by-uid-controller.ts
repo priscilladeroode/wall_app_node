9b11ad198bf1392d4a332ad4e844259895754b56
Priscilla de Roode <priscilladeroode@gmail.com>
2022-01-22 22:11:02 -0300

    feat: ensure LoadPostsByUidController calls Validation with correct values

--- src/presentation/controllers/posts/load-posts-by-uid-controller.ts
+++ src/presentation/controllers/posts/load-posts-by-uid-controller.ts
@@ -1,12 +1,20 @@
 import { LoadPostsByUidUseCase } from '../../../domain/usecases/posts/load-posts-by-uid-usecase'
-import { ok, serverError } from '../../helpers/http'
+import { badRequest, ok, serverError } from '../../helpers/http'
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
+import { Validation } from '../../protocols/validation'
 
 export class LoadPostsByUidController implements Controller {
-  constructor (private readonly loadPostsByUidUseCase: LoadPostsByUidUseCase) {}
+  constructor (
+    private readonly loadPostsByUidUseCase: LoadPostsByUidUseCase,
+    private readonly validation: Validation
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      const error = this.validation.validate(httpRequest.body)
+      if (error) {
+        return badRequest(error)
+      }
       const { userId } = httpRequest.body
       const posts = await this.loadPostsByUidUseCase.loadByUid({ uid: userId })
       return ok(posts)
