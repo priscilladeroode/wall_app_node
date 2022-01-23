13a84189d86dcd4c13ea7c7261509ff77c194dd6
Priscilla de Roode <priscilladeroode@gmail.com>
2022-01-22 22:58:31 -0300

    feat: ensure loadByUid returns a list of posts of the user on descending order on success

--- src/infra/db/mongodb/post-mongo-repository.ts
+++ src/infra/db/mongodb/post-mongo-repository.ts
@@ -1,15 +1,21 @@
+import { ObjectId } from 'mongodb'
 import {
   AddPostRequestModel,
   AddPostResponseModel,
+  LoadPostsByUidRequestModel,
   LoadPostsResponseModel
 } from '../../../data/models/posts'
 import { AddPostRepository } from '../../../data/protocols/db/posts/add-post-repository'
 import { LoadAllPostsRepository } from '../../../data/protocols/db/posts/load-all-posts-repository'
+import { LoadPostsByUidRepository } from '../../../data/protocols/db/posts/load-posts-by-uid-repository'
 import { MongoHelper } from '../../helpers/mongo-helper'
 import { QueryBuilder } from '../../helpers/query-builder-helper'
 
 export class PostMongoRepository
-implements AddPostRepository, LoadAllPostsRepository {
+implements
+    AddPostRepository,
+    LoadAllPostsRepository,
+    LoadPostsByUidRepository {
   async add (post: AddPostRequestModel): Promise<AddPostResponseModel> {
     const collection = MongoHelper.getCollection('posts')
     const inserted = await collection.insertOne(post)
@@ -22,6 +28,40 @@ implements AddPostRepository, LoadAllPostsRepository {
   async loadAll (): Promise<LoadPostsResponseModel> {
     const collection = MongoHelper.getCollection('posts')
     const query = new QueryBuilder()
+      .lookup({
+        from: 'users',
+        foreignField: '_id',
+        localField: 'uid',
+        as: 'users'
+      })
+      .unwind({ path: '$users' })
+      .project({
+        id: '$_id',
+        title: '$title',
+        content: '$content',
+        createdBy: '$users.name',
+        createdAt: { $toDate: '$_id' }
+      })
+      .sort({
+        _id: -1
+      })
+      .build()
+
+    const result = await collection.aggregate(query).toArray()
+    if (result.length > 0) {
+      return MongoHelper.mapCollection(result)
+    }
+    return result as LoadPostsResponseModel
+  }
+
+  async loadByUid (
+    model: LoadPostsByUidRequestModel
+  ): Promise<LoadPostsResponseModel> {
+    const collection = MongoHelper.getCollection('posts')
+    const query = new QueryBuilder()
+      .match({
+        uid: new ObjectId(model.uid)
+      })
       .lookup({
         from: 'users',
         foreignField: '_id',
