2d4eb7f1bc2123b6c388755f8f17510810a61597
Priscilla de Roode <priscilladeroode@gmail.com>
2022-01-22 21:21:48 -0300

    feat: ensure on get posts with accessToken returns 200

--- src/main/routes/post-routes.ts
+++ src/main/routes/post-routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express'
 import { adaptMiddleware } from '../adapters/express-middleware-adapter'
 import { adaptRoute } from '../adapters/express-route-adapter'
 import { makeAddPostController } from '../factories/controllers/posts/add-post-controller-factory'
+import { makeLoadAllPostsController } from '../factories/controllers/posts/load-all-posts-controller-factory'
 import { makeAuth } from '../factories/middlewares/auth-middleware'
 
 export default (router: Router): void => {
@@ -9,5 +10,10 @@ export default (router: Router): void => {
     '/posts',
     adaptMiddleware(makeAuth()),
     adaptRoute(makeAddPostController())
+  )
+  router.get(
+    '/posts',
+    adaptMiddleware(makeAuth()),
+    adaptRoute(makeLoadAllPostsController())
   )
 }
