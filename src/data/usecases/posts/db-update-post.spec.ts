87e447f013bee08ca8b98eb076f46bccbbf75f77
Priscilla de Roode <priscilladeroode@gmail.com>
2022-01-23 01:31:02 -0300

    test: ensure DBUpdatePost calls UpdatePostRepository with correct values

---
+++ src/data/usecases/posts/db-update-post.spec.ts
@@ -0,0 +1,65 @@
+import {
+  UpdatePostRequestModel,
+  UpdatePostResponseModel
+} from '../../models/posts'
+import faker from 'faker'
+import { UpdatePostRequestEntity } from '../../../domain/entities/posts'
+import { UpdatePostRepository } from '../../protocols/db/posts/update-post-repository'
+import { DBUpdatePost } from './db-update-post'
+
+interface SutTypes {
+  sut: DBUpdatePost
+  updatePostRepositoryStub: UpdatePostRepository
+}
+
+const title = faker.lorem.sentence()
+const content = faker.lorem.paragraphs()
+const uid = faker.datatype.uuid()
+const id = faker.datatype.uuid()
+
+const request: UpdatePostRequestEntity = {
+  id,
+  title,
+  content,
+  uid
+}
+
+const updatePostRequestModel: UpdatePostRequestModel = {
+  id,
+  title,
+  content,
+  uid
+}
+
+const updatePostResponseModel: UpdatePostResponseModel = { id }
+
+const makeUpdatePostRepository = (): UpdatePostRepository => {
+  class UpdatePostRepositoryStub implements UpdatePostRepository {
+    async update (
+      post: UpdatePostRequestModel
+    ): Promise<UpdatePostResponseModel> {
+      return await Promise.resolve(updatePostResponseModel)
+    }
+  }
+  return new UpdatePostRepositoryStub()
+}
+
+const makeSut = (): SutTypes => {
+  const updatePostRepositoryStub = makeUpdatePostRepository()
+  const sut = new DBUpdatePost(updatePostRepositoryStub)
+  return {
+    sut,
+    updatePostRepositoryStub
+  }
+}
+
+describe('DBUpdatePost', () => {
+  describe('UpdatePostRepository', () => {
+    test('Should call UpdatePostRepository with correct values', async () => {
+      const { sut, updatePostRepositoryStub } = makeSut()
+      const addSpy = jest.spyOn(updatePostRepositoryStub, 'update')
+      await sut.update(request)
+      expect(addSpy).toHaveBeenCalledWith(updatePostRequestModel)
+    })
+  })
+})
