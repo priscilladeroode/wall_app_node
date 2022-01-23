7ef7aa28c60f83402bf0044f2f8515e56eef042e
Priscilla de Roode <priscilladeroode@gmail.com>
2022-01-23 15:03:14 -0300

    feat: ensure deleteById posts it returns 200

--- src/main/routes/post-routes.ts
+++ src/main/routes/post-routes.ts
@@ -2,10 +2,13 @@ import { Router } from 'express'
 import { adaptMiddleware } from '../adapters/express-middleware-adapter'
 import { adaptRoute } from '../adapters/express-route-adapter'
 import { makeAddPostController } from '../factories/controllers/posts/add-post-controller-factory'
+import { makeDeletePostByIdController } from '../factories/controllers/posts/delete-post-by-id-controller-factory'
 import { makeLoadAllPostsController } from '../factories/controllers/posts/load-all-posts-controller-factory'
 import { makeAuth } from '../factories/middlewares/auth-middleware'
 
 export default (router: Router): void => {
+  router.get('/posts', adaptRoute(makeLoadAllPostsController()))
+  router.get('/postsByUser/:userId', adaptRoute(makeLoadAllPostsController()))
   router.post(
     '/posts',
     adaptMiddleware(makeAuth()),
@@ -16,6 +19,9 @@ export default (router: Router): void => {
     adaptMiddleware(makeAuth()),
     adaptRoute(makeLoadAllPostsController())
   )
-  router.get('/posts', adaptRoute(makeLoadAllPostsController()))
-  router.get('/postsByUser/:userId', adaptRoute(makeLoadAllPostsController()))
+  router.delete(
+    '/deleteById/:id',
+    adaptMiddleware(makeAuth()),
+    adaptRoute(makeDeletePostByIdController())
+  )
 }
