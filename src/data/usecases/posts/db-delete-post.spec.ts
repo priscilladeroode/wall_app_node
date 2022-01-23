199a507d031a02b49579d15649729c6642dee907
Priscilla de Roode <priscilladeroode@gmail.com>
2022-01-23 14:07:57 -0300

    test: ensure DBDeletePost calls CheckPostExistsByIdRepository with correct values

---
+++ src/data/usecases/posts/db-delete-post.spec.ts
@@ -0,0 +1,59 @@
+import { CheckPostExistsByIdRepository } from '../../protocols/db/posts/check-post-exists-by-id'
+import { DBDeletePost } from './db-delete-post'
+
+import faker from 'faker'
+import { CheckPostExistsResponseModel } from '../../models/posts'
+import { DeletePostRequestEntity } from '../../../domain/entities/posts'
+
+interface SutTypes {
+  sut: DBDeletePost
+  checkPostExistsByIdRepositoryStub: CheckPostExistsByIdRepository
+}
+
+const title = faker.lorem.sentence()
+const content = faker.lorem.paragraphs()
+const uid = faker.datatype.uuid()
+const id = faker.datatype.uuid()
+
+const request: DeletePostRequestEntity = { id, uid }
+
+const checkPostExistsResponseModel: CheckPostExistsResponseModel = {
+  id,
+  title,
+  content,
+  uid
+}
+
+const makeCheckPostExistsByIdRepository = (): CheckPostExistsByIdRepository => {
+  class CheckPostExistsByIdRepositoryStub
+  implements CheckPostExistsByIdRepository {
+    async checkById (id: string): Promise<CheckPostExistsResponseModel> {
+      return await Promise.resolve(checkPostExistsResponseModel)
+    }
+  }
+  return new CheckPostExistsByIdRepositoryStub()
+}
+
+const makeSut = (): SutTypes => {
+  const checkPostExistsByIdRepositoryStub = makeCheckPostExistsByIdRepository()
+
+  const sut = new DBDeletePost(checkPostExistsByIdRepositoryStub)
+  return {
+    sut,
+    checkPostExistsByIdRepositoryStub
+  }
+}
+
+describe('DBDeletePost', () => {
+  describe('CheckPostExistsByIdRepository', () => {
+    test('Should call CheckPostExistsByIdRepository with correct values', async () => {
+      const { sut, checkPostExistsByIdRepositoryStub } = makeSut()
+      const checkByIdSpy = jest.spyOn(
+        checkPostExistsByIdRepositoryStub,
+        'checkById'
+      )
+      await sut.delete(request)
+      expect(checkByIdSpy).toHaveBeenCalledWith(id)
+    })
+  })
+})
