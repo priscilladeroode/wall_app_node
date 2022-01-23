83a2b47c66f3d28149768f0871847eb5c4908a8b
Priscilla de Roode <priscilladeroode@gmail.com>
2022-01-23 16:56:50 -0300

    test: ensure LoadPostsByIdController calls Validation with correct values

---
+++ src/presentation/controllers/posts/load-post-by-id-controller.spec.ts
@@ -0,0 +1,70 @@
+import faker from 'faker'
+
+import { PostEntity } from '../../../domain/entities/posts'
+import { LoadPostsByIdUseCase } from '../../../domain/usecases/posts/load-post-by-id-usecase'
+import { HttpRequest } from '../../protocols'
+import { Validation } from '../../protocols/validation'
+import { LoadPostsByIdController } from './load-post-by-id-controller'
+
+type SutTypes = {
+  sut: LoadPostsByIdController
+  validationStub: Validation
+  loadPostsByIdUseCaseStub: LoadPostsByIdUseCase
+}
+
+const title = faker.lorem.sentence()
+const content = faker.lorem.paragraphs()
+const id = faker.datatype.uuid()
+const createdAt = faker.datatype.datetime()
+const createdBy = faker.name.findName()
+
+const postEntity: PostEntity = { id, title, content, createdAt, createdBy }
+
+const request: HttpRequest = { body: { id } }
+
+const makeValidation = (): Validation => {
+  class ValidationStub implements Validation {
+    validate (input: any): Error {
+      return null
+    }
+  }
+  return new ValidationStub()
+}
+
+const makeLoadPostsByIdUseCaseStub = (): LoadPostsByIdUseCase => {
+  class LoadPostsByIdUseCaseStub implements LoadPostsByIdUseCase {
+    async loadById (id: string): Promise<PostEntity> {
+      return await Promise.resolve(postEntity)
+    }
+  }
+  return new LoadPostsByIdUseCaseStub()
+}
+
+const makeSut = (): SutTypes => {
+  const validationStub = makeValidation()
+  const loadPostsByIdUseCaseStub = makeLoadPostsByIdUseCaseStub()
+  const sut = new LoadPostsByIdController(
+    loadPostsByIdUseCaseStub,
+    validationStub
+  )
+  return {
+    sut,
+    validationStub,
+    loadPostsByIdUseCaseStub
+  }
+}
+
+export const throwError = (): never => {
+  throw new Error()
+}
+
+describe('LoadPostsByIdController', () => {
+  describe('Validation', () => {
+    test('Shoud call Validation with correct values', async () => {
+      const { sut, validationStub } = makeSut()
+      const validateSpy = jest.spyOn(validationStub, 'validate')
+      await sut.handle(request)
+      expect(validateSpy).toHaveBeenCalledWith(request.body)
+    })
+  })
+})
