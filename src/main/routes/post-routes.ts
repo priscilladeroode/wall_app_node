0dcf289de7b6fa43b4217527dab7b3e4093d0aa6
Priscilla de Roode <priscilladeroode@gmail.com>
2022-01-30 01:48:30 -0300

    fix: fix wrong controller on routes

--- src/main/routes/post-routes.ts
+++ src/main/routes/post-routes.ts
@@ -3,13 +3,16 @@ import { adaptMiddleware } from '../adapters/express-middleware-adapter'
 import { adaptRoute } from '../adapters/express-route-adapter'
 import { makeAddPostController } from '../factories/controllers/posts/add-post-controller-factory'
 import { makeDeletePostByIdController } from '../factories/controllers/posts/delete-post-by-id-controller-factory'
+import { makeLoadPostsByUidController } from '../factories/controllers/posts/list-posts-by-uid-controller-factory'
 import { makeLoadAllPostsController } from '../factories/controllers/posts/load-all-posts-controller-factory'
+import { makeLoadPostByIdController } from '../factories/controllers/posts/load-post-by-id-controller-factory'
+import { makeUpdatePostController } from '../factories/controllers/posts/update-post-controller-factory'
 import { makeAuth } from '../factories/middlewares/auth-middleware'
 
 export default (router: Router): void => {
   router.get('/posts', adaptRoute(makeLoadAllPostsController()))
-  router.get('/posts/:id', adaptRoute(makeLoadAllPostsController()))
-  router.get('/postsByUser/:userId', adaptRoute(makeLoadAllPostsController()))
+  router.get('/posts/:id', adaptRoute(makeLoadPostByIdController()))
+  router.get('/postsByUser/:userId', adaptRoute(makeLoadPostsByUidController()))
   router.post(
     '/posts',
     adaptMiddleware(makeAuth()),
@@ -18,7 +21,7 @@ export default (router: Router): void => {
   router.put(
     '/posts/:id',
     adaptMiddleware(makeAuth()),
-    adaptRoute(makeLoadAllPostsController())
+    adaptRoute(makeUpdatePostController())
   )
   router.delete(
     '/deleteById/:id',
