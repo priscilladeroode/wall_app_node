2626230e2812033d8a0f08d09f38d67968aaee89
Priscilla de Roode <priscilladeroode@gmail.com>
2022-01-30 01:57:47 -0300

    test: add a custom error

--- tests/presentation/mocks/mock-validation.ts
+++ tests/presentation/mocks/mock-validation.ts
@@ -1,10 +1,11 @@
 import { Validation } from '@/presentation/protocols/validation'
+import { CustomError } from '../protocols/custom_error'
 
 export class ValidationSpy implements Validation {
-  error: Error = null
+  error: CustomError = null
   input: any
 
-  validate (input: any): Error {
+  validate (input: any): CustomError {
     this.input = input
     return this.error
   }
