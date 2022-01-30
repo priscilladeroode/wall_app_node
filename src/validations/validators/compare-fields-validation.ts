8d22ad5352c026d1fa88097ae769c681e13b0eb9
Priscilla de Roode <priscilladeroode@gmail.com>
2022-01-30 02:10:42 -0300

    feat: add FieldNotMatchError

--- src/validations/validators/compare-fields-validation.ts
+++ src/validations/validators/compare-fields-validation.ts
@@ -1,4 +1,4 @@
-import { InvalidParamError } from '@/presentation/errors'
+import { FieldNotMatchError } from '@/presentation/errors/fields-not-match-error'
 import { CustomError } from '@/presentation/protocols/custom_error'
 import { Validation } from '@/presentation/protocols/validation'
 
@@ -10,7 +10,7 @@ export class CompareFieldsValidation implements Validation {
 
   validate (input: any): CustomError {
     if (input[this.fieldName] !== input[this.fieldToCompare]) {
-      return new InvalidParamError(this.fieldToCompare)
+      return new FieldNotMatchError(this.fieldName, this.fieldToCompare)
     }
   }
 }
