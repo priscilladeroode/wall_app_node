d85dff47cb35ea8cfb9bb8b2db8b38887f89e7c3
priscilladeroode <53240218+priscilladeroode@users.noreply.github.com>
2022-02-02 22:12:53 -0300

    Delete env.ts

--- src/main/config/env.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  mongoUrl:
-    process.env.MONGO_URL || 'mongodb://localhost:27017/wall-app-backend',
-  port: process.env.PORT || 5050,
-  jwtSecret: process.env.JWT_SECRET || 'PRtA1988*2305=pEFG',
-  sendGrid:
-    process.env.SENDGRID_API_KEY ||
-    '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'
-}
